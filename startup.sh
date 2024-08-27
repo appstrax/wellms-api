91bd307ee9941720bba5b18ac481dc3660f9e017
Miki von Ketelhodt <vonketelhodtm@gmail.com>
2024-08-27 19:49:28 +0200

    startup.sh

--- startup.sh
+++ startup.sh
@@ -6,8 +6,22 @@ mkdir -p ${APP_STORAGE_PATH}/storage/framework
 mkdir -p ${APP_STORAGE_PATH}/storage/framework/sessions
 mkdir -p ${APP_STORAGE_PATH}/storage/framework/views
 mkdir -p ${APP_STORAGE_PATH}/storage/framework/cache
+mkdir -p ${APP_STORAGE_PATH}/storage/logs
+mkdir -p ${APP_STORAGE_PATH}/storage/app
 
 # Set the permissions
 chmod -R 755 ${APP_STORAGE_PATH}/storage/framework 
+chmod -R 755 ${APP_STORAGE_PATH}/storage/app 
+chmod -R 755 ${APP_STORAGE_PATH}/storage/logs 
+
+FILE=${APP_STORAGE_PATH}/storage/oauth-private.key
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."     
+else 
+    echo "$FILE does not exist. Generating app keys, passport keys and passport client"
+    php artisan key:generate --force --no-interaction
+    php artisan passport:keys --force --no-interaction 
+    php artisan passport:client --personal --no-interaction
+fi
 
 php artisan clear-compiled && php artisan optimize:clear && php artisan config:cache && php artisan config:clear && php artisan view:clear && php artisan cache:clear && php artisan migrate --force && php artisan storage:link && php docker/envs/envs.php && /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
